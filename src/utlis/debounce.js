6c7bc3254f602ab8dfd446fed7cdbdde1a56839a
靳少辉 <1051131737@qq.com>
2022-04-04 21:41:02 +0800

    格式化方法代码

--- src/utlis/debounce.js
+++ src/utlis/debounce.js
@@ -1,9 +1,9 @@
-export function debounce(fun,time=500) {
-	let timer
-	return function (...args) {
-		if(timer)clearTimeout(timer)
-		timer = setTimeout(() => {
-			fun.apply(this,args)
-		},time)
-	}
+export function debounce(fun, time = 500) {
+  let timer
+  return function(...args) {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fun.apply(this, args)
+    }, time)
+  }
 }
