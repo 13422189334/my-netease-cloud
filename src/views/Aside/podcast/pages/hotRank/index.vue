07a74d0112635fb02a19ddaf1e650b0dba15f701
靳少辉 <1051131737@qq.com>
2022-04-21 14:34:18 +0800

    播客 - 热门播客榜 - 代码调整

---
+++ src/views/Aside/podcast/pages/hotRank/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <skeleton1
+    :count="8"
+    :loading="songArray.length"
+    :image="{ width: '70px', height: '70px' }"
+    :margin="{ width: '95%', marginLeft: '5px' }"
+    :row="1"
+  >
+    <topList :array="songArray" @toDetail="toDetail" />
+  </skeleton1>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import topList from '../components/topList.vue'
+import { getNewTopList } from '@/network/radio.js'
+import { formatNewData } from '@/utlis/formatData.js'
+import { ref, onMounted } from 'vue'
+
+const songArray = ref([])
+onMounted(() => {
+  getNewTopList('hot').then(res => {
+    songArray.value = formatNewData(res.data.toplist.slice(0, 30))
+  })
+})
+
+const router = useRouter()
+const toDetail = id => {
+  router.push(`/program?id=${id}`)
+}
+</script>
