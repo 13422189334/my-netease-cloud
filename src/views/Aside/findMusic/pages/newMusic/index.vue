21d90beffd4d52f5ecf6e3000725a89f81f0bcb0
靳少辉 <1051131737@qq.com>
2022-04-22 17:43:53 +0800

    发现音乐 - 最新音乐 - 新歌速递页面位置调整

--- src/views/Aside/findMusic/pages/newMusic/index.vue
+++ src/views/Aside/findMusic/pages/newMusic/index.vue
@@ -1,20 +1,23 @@
 <template>
   <header>
     <nav class="nav">
-      <router-link to="/findMusic/newMusic" v-slot="{scope}">
-        <el-button class="button" round size="small" :class="{active:$route.path === '/findMusic/newMusic'}">新歌速递</el-button>
+      <router-link to="/findMusic/newMusic/song" v-slot="{scope}">
+        <el-button class="button" round size="small" :class="{active:$route.path === '/findMusic/newMusic/song'}">新歌速递
+        </el-button>
       </router-link>
-      <router-link to="/findMusic/newMusic/newAlbum">
-        <el-button class="button" round size="small" :class="{active:$route.path === '/findMusic/newMusic/newAlbum'}">新碟上架</el-button>
+      <router-link to="/findMusic/newMusic/disc">
+        <el-button class="button" round size="small" :class="{active:$route.path === '/findMusic/newMusic/disc'}">
+          新碟上架
+        </el-button>
       </router-link>
     </nav>
   </header>
   <br>
   <br>
   <router-view v-slot="{Component}">
-      <keep-alive>
-        <component :is="Component"></component>
-      </keep-alive>
+    <keep-alive>
+      <component :is="Component"></component>
+    </keep-alive>
   </router-view>
 </template>
 
@@ -23,20 +26,23 @@
 </script>
 
 <style scoped lang="less">
-.active{
-  background: silver;
-  color: whitesmoke;
-}
-header{
-  margin-top: 60px;
-  display: flex;
-  justify-content: center;
-  .nav{
-    border-radius: 20px;
-    border: 1px solid #ededed;
-    .button{
-      width: 150px;
+  .active {
+    background: silver;
+    color: whitesmoke;
+  }
+
+  header {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+
+    .nav {
+      border-radius: 20px;
+      border: 1px solid #ededed;
+
+      .button {
+        width: 150px;
+      }
     }
   }
-}
 </style>
