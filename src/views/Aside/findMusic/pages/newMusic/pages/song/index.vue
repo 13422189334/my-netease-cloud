7783b01e59ddae5da35c8c84610efa2326721fc5
靳少辉 <1051131737@qq.com>
2022-04-24 14:33:35 +0800

    发现音乐 - 最新音乐 - 新歌速递页面位置调整

--- src/views/Aside/findMusic/pages/newMusic/pages/song/index.vue
+++ src/views/Aside/findMusic/pages/newMusic/pages/song/index.vue
@@ -2,18 +2,28 @@
   <header class="header">
     <navTabs @change="change" />
     <div>
-      <el-button type="danger" size="mini" icon="el-icon-video-play" round>播放全部</el-button>
-      <el-button type="success" size="mini" icon="el-icon-folder-add" round>收藏全部</el-button>
+      <el-button
+        v-for="(button, bIndex) in buttons"
+        :key="bIndex"
+        :type="button.type"
+        :size="button.size"
+        :icon="button.icon"
+        :disabled="button.disabled"
+        @click="button.handle"
+        round
+      >
+        {{ button.name }}
+      </el-button>
     </div>
   </header>
-  <skeleton1 :count="8" :loading="newSong.length" :image="{width:'70px',height:'70px'}" :row="1" show>
+  <skeleton1 :count="8" :loading="songArray.length" :image="{width:'70px',height:'70px'}" :row="1" show>
     <nav class="item">
       <el-row
-        v-for="(item,index) in newSong"
+        v-for="(item,index) in songArray"
         :key="item.id"
         class="item-newSong"
         align="middle"
-        @dblclick="current(item,index)"
+        @dblclick="current(item, index)"
       >
         <el-col :span="1">
           <div style="margin-left: 10px;">
@@ -42,35 +52,43 @@
       </el-row>
     </nav>
   </skeleton1>
-
 </template>
 
 <script setup>
-import eventbus from '@/utlis/eventbus.js'
 import navTabs from '../../components/navTabs.vue'
+import { computed, reactive } from 'vue'
+import { VideoPlay, FolderAdd } from '@element-plus/icons-vue'
+import eventbus from '@/utlis/eventbus.js'
 import { getNewMusic } from '@/network/song.js'
-import { computed, ref } from 'vue'
 import { formatNewMusic } from '@/utlis/formatData.js'
-
 import { useStore } from 'vuex'
 
-const store = useStore()
-const length = ref(0)
-const newSong = computed(() => store.state.songDetail.songArray.slice(0, 20))
+// 默认为当前播放歌单的前20首
+const songArray = computed(() => store.state.songDetail.songArray.slice(0, 20))
 
-function getNewMusicData(res) {
-  store.commit('setSongMusic', formatNewMusic(res?.data?.data))
-}
+// 按钮集合
+const buttons = reactive([
+  { type: 'danger', size: 'mini', icon: VideoPlay, name: '播放全部', disabled: false, handle: () => current(songArray.value[0], 0) },
+  { type: 'success', size: 'mini', icon: FolderAdd, name: '收藏全部', disabled: true }
+])
 
+/**
+ * 播放歌曲  全部播放：默认第一个
+ * @param item
+ * @param index
+ */
 const current = (item, index) => {
   store.commit('setSongDetail', item)
   store.commit('play', index)
   eventbus.emit('playMusic')
 }
 
+const store = useStore()
+
+// 调整分类
 const change = value => {
   getNewMusic(value.type).then(res => {
-    getNewMusicData(res)
+    store.commit('setSongMusic', formatNewMusic(res?.data?.data))
   })
 }
 
