02014086873415bbaf177347a6dd730258938fa6
靳少辉 <1051131737@qq.com>
2022-04-25 16:32:27 +0800

    我的收藏 - 中间页调整

---
+++ src/views/Aside/myCollect/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-menu :default-active="$route.path" mode="horizontal" router>
+    <el-menu-item v-for="menu in menus" :index="menu.path" :key="menu.path">{{ menu.name }}</el-menu-item>
+  </el-menu>
+  <router-view v-slot="{Component}">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const menus = ref([
+  { path: '/myCollect/album', name: '专辑' },
+  { path: '/myCollect/singer', name: '歌手' },
+  { path: '/myCollect/video', name: '视频' }
+])
+</script>
