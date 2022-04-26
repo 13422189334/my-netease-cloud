6393a3cd7e072fa262ff960f50ac22817ab3befe
靳少辉 <1051131737@qq.com>
2022-04-26 17:45:25 +0800

    视频MV - 中间页调整

---
+++ src/views/Aside/video/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-menu router :default-active="$route.path" mode="horizontal">
+    <el-menu-item v-for="menu in menus" :key="menu.path" :index="menu.path">{{ menu.name }}</el-menu-item>
+  </el-menu>
+  <router-view v-slot="{Component}">
+    <keep-alive>
+      <component :is="Component"></component>
+    </keep-alive>
+  </router-view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const menus = ref([
+  { name: '视频', path: '/video/video' },
+  { name: 'MV', path: '/video/mv' },
+  { name: '全部MV', path: '/video/allMV' },
+  { name: '全部MV', path: '/video/rank' }
+])
+</script>
