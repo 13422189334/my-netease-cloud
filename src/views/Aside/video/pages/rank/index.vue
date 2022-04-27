ab1d2e2a4c1ea40fe913210450c9ed2e98cb74ba
靳少辉 <1051131737@qq.com>
2022-04-27 13:56:28 +0800

    视频MV - MV排行榜

--- src/views/Aside/video/pages/rank/index.vue
+++ src/views/Aside/video/pages/rank/index.vue
@@ -7,62 +7,94 @@
       <span
         v-for="(item,index) in tags"
         :key="index"
-        :class="{active:current === item}"
-        @click="tagChange(item)"
-      >{{ item }}</span></div>
+        :class="{ active: current === item }"
+        @click="change(item)"
+      >
+        {{ item }}
+      </span>
+    </div>
   </header>
-  <skeletonTop :count="10" :loading="mvTopList.length">
-    <section>
-      <main v-for="(item,index) in mvTopList" :key="item.id" class="mvTopList" @click="goMvDetail(item.id)">
-        <div :class="{active:index < 3}" style="width: 30px; font-size: 25px; margin-right: 10px; font-weight: 900;">
-          {{ index+1 < 10 ? `0${index+1}` : index+1 }}
+  <el-skeleton
+    :loading="!Boolean(mvArray.length)"
+    :count="1"
+    :animated="true"
+  >
+    <template #template>
+      <section>
+        <div v-for="item in 10" :key="item" class="mvTopList">
+          <el-skeleton-item variant="h1" class="num" />
+          <div class="skeleton-item-box">
+            <el-skeleton-item variant="image" class="skeleton-item-image" />
+            <div class="right">
+              <el-skeleton-item variant="h1" class="skeleton-item-p" />
+              <el-skeleton-item variant="h1" class="skeleton-item-p" />
+            </div>
+          </div>
         </div>
-        <mvCover
-          :image="item.cover"
-          :count="item.score"
-          :label="item.artists"
-          :name="item.name"
-        />
-      </main>
-    </section>
-  </skeletonTop>
-  <br>
-  <el-divider v-if="isShow && mvTopList.length > 9" @click="loading">点击加载更多</el-divider>
+      </section>
+    </template>
+    <template #default>
+      <section>
+        <main v-for="(item,index) in mvArray" :key="item.id" class="mvTopList" @click="toMvDetail(item.id)">
+          <div :class="{ active: index < 3 }" class="num">
+            {{ index &lt; 9 ? `0${index + 1}` : index + 1 }}
+          </div>
+          <verticalCover
+            :image="item.cover"
+            :count="item.score"
+            :label="item.artists"
+            :name="item.name"
+          />
+        </main>
+      </section>
+    </template>
+  </el-skeleton>
+  <el-divider v-if="isShow && mvArray.length > 9" @click="loading">点击加载更多</el-divider>
   <el-divider v-else>没有数据了</el-divider>
 </template>
 
 <script setup>
-import skeletonTop from '../../children/skeletonTop.vue'
+import verticalCover from '@/views/Aside/video/components/verticalCover.vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import mvCover from '../../children/mvCover.vue'
 import { getMvTopList } from '@/network/video.js'
-import { onMounted } from 'vue'
-import { ref } from 'vue'
 
 const isShow = ref(true)
-const current = ref('内地')
-const tags = ref(['内地', '港台', '欧美', '日本', '韩国'])
-const mvTopList = ref([])
-onMounted(async() => {
-  const res = await getMvTopList(current.value)
-  mvTopList.value = res.data.data
-})
-const tagChange = item => {
-  current.value = item
+const current = ref('内地') // 当前分类
+const offset = ref(0)
+const tags = ref(['内地', '港台', '欧美', '日本', '韩国']) // 右侧分类
+const mvArray = ref([])
+
+const getMvList = () => {
   getMvTopList(current.value).then(res => {
-    mvTopList.value = res.data.data
+    mvArray.value = res.data.data
   })
 }
+
+onMounted(() => {
+  getMvList()
+})
+
+/**
+ * 切换分类
+ * @param item
+ */
+const change = item => {
+  current.value = item
+  offset.value = 0
+  getMvList()
+}
+
 const router = useRouter()
-const goMvDetail = id => {
+const toMvDetail = id => {
   router.push(`/videoDetail?id=${id}`)
 }
-let offset = 0
+
 const loading = () => {
-  offset += 10
-  getMvTopList(current.value, offset).then(res => {
-    mvTopList.value.push(...res.data.data)
-  }).catch(err => {
+  offset.value += 10
+  getMvTopList(current.value, offset.value).then(res => {
+    mvArray.value.push(...res.data.data)
+  }).catch(() => {
     isShow.value = false
   })
 }
@@ -95,11 +127,35 @@ const loading = () => {
     .mvTopList {
       width: 45%;
       height: 130px;
-      margin-top: 20px;
+      margin-bottom: 20px;
       display: flex;
       justify-content: flex-start;
       align-items: center;
+
+      .num {
+        width: 30px;
+        font-size: 25px;
+        margin-right: 10px;
+        font-weight: 900;
+      }
+
+      .skeleton-item-box {
+        display: flex;
+        flex-direction: row;
+        .skeleton-item-image {
+          width: 292px;
+          height: 130px;
+        }
+        .skeleton-item-p {
+          width: 321px;
+        }
+        .right {
+          margin-left: 10px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+        }
+      }
     }
   }
-
 </style>
