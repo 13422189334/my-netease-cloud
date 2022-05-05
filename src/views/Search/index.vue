5a68d523c585c4ad906aa35a0576b32bc131d46c
靳少辉 <1051131737@qq.com>
2022-05-05 17:30:37 +0800

    搜索页 - 搜索中间页 - 代码重构

---
+++ src/views/Search/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-menu :default-active="$route.path" mode="horizontal" router>
+    <el-menu-item v-for="menu in menus" :key="menu" :index="menu.path">{{ menu.name }}</el-menu-item>
+  </el-menu>
+  <router-view v-slot="{Component}">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const menus = ref([
+  { name: '单曲', path: '/search/song' },
+  { name: '歌手', path: '/search/singer' },
+  { name: '专辑', path: '/search/album' },
+  { name: '视频', path: '/search/video' },
+  { name: '歌单', path: '/search/songMenu' }
+])
+</script>
