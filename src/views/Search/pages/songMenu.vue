aa9186529628a57cfca72d01591545b6548d6f8e
靳少辉 <1051131737@qq.com>
2022-05-05 17:30:19 +0800

    搜索页 - 歌单 - 代码重构

--- src/views/Search/pages/songMenu.vue
+++ src/views/Search/pages/songMenu.vue
@@ -1,65 +1,80 @@
 <template>
-  <skeleton1 :count="10" :loading="songMenu.length" :image="{width:'70px',height:'70px'}" :row="1" :margin="{width: '1111px',marginLeft:'10px'}">
-      <div class="item" v-for="item in songMenu" :key="item.id" @click="goDetail(item.id)">
+  <skeleton1
+    :count="10"
+    :loading="songMenu.length"
+    :image="{ width: '70px', height: '70px' }"
+    :row="1"
+    :margin="{ width: '95%', marginLeft: '10px' }"
+  >
+    <div v-for="item in songMenu" :key="item.id" class="item" @click="toDetail(item.id)">
       <div class="left">
-         <el-avatar shape="square" :size="70" :src="item.picUrl"></el-avatar>
-         <span class="name">{{item.name}}</span>
+        <el-avatar shape="square" :size="70" :src="item.picUrl" />
+        <span class="name">{{ item.name }}</span>
       </div>
-      <div style="color: #656161;">{{item.num}}首</div>
-      <div class="right" style="color: #656161;">
-          {{item.label}}
+      <div>{{ item.num }}首</div>
+      <div class="right">
+        {{ item.label }}
       </div>
     </div>
   </skeleton1>
 </template>
 
 <script setup>
-import {computed,ref} from "vue";
-import {useRouter} from "vue-router";
-import {useStore} from "vuex";
-import {getSearchResult} from "@/network/search.js";
-import {formatSongMenuData} from '@/utlis/formatData.js'
+import { computed, ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { getSearchResult } from '@/network/search.js'
+import { formatSongMenuData } from '@/utlis/formatData.js'
 
-let songMenu = ref([])
+const songMenu = ref([])
 
 const store = useStore()
 const keywords = computed(() => store.state.songDetail.keywords)
 
-getSearchResult({keywords:keywords.value,type:1000}).then(res => {
-  songMenu.value = formatSongMenuData(res.data.result.playlists)
+onMounted(() => {
+  getSearchResult({ keywords: keywords.value, type: 1000 }).then(res => {
+    songMenu.value = formatSongMenuData(res.data.result.playlists)
+  })
 })
+
 const router = useRouter()
-const goDetail = id => {
-  store.dispatch('getSongList',id)
+const toDetail = id => {
+  store.dispatch('getSongList', id)
   router.push('/songDetail')
 }
 </script>
 
 <style scoped lang="less">
-.item{
-  width: 100%;
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 10px;
-  &:hover{
-    background: #ededed;
-  }
-  .left{
-    width: 50%;
+  .item {
+    width: 100%;
+    height: 80px;
     display: flex;
     align-items: center;
-  }
-  .name{
-    padding-left: 10px;
-  }
-  .right{
-    width: 20%;
-    i{
-      font-size: 30px;
-      color: red;
+    justify-content: space-between;
+    margin-top: 10px;
+    color: #656161;
+
+    &:hover {
+      background: #ededed;
+    }
+
+    .left {
+      width: 50%;
+      display: flex;
+      align-items: center;
+    }
+
+    .name {
+      padding-left: 10px;
+    }
+
+    .right {
+      width: 20%;
+
+      i {
+        font-size: 30px;
+        color: red;
+      }
     }
   }
-}
 </style>
