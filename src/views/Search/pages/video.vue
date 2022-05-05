20f37281eb2d8620369b62b46b553bb61f48d7cf
靳少辉 <1051131737@qq.com>
2022-05-05 17:30:04 +0800

    搜索页 - 视频 - 代码重构

--- src/views/Search/pages/video.vue
+++ src/views/Search/pages/video.vue
@@ -1,41 +1,70 @@
 <template>
-   <skeleton2 :count="12" :loading="video.length" :size="{width:'286px',height: '150px'}">
-    <div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
-      <videoCover :videoArray="video" @goDetail="goDetail"/>
-    </div>
-   </skeleton2>
+  <el-skeleton :loading="!Boolean(videoArray.length)" :count="1" :animated="true">
+    <template #template>
+      <div class="video-box">
+        <div v-for="item in 16" :key="item">
+          <el-skeleton-item variant="image" class="skeleton-item-image" />
+          <el-skeleton-item variant="p" class="skeleton-item-p" />
+          <el-skeleton-item variant="p" class="skeleton-item-p" />
+        </div>
+      </div>
+    </template>
+    <template #default>
+      <div class="video-box">
+        <horizontalCover :width="'285px'" :video-array="videoArray" @toDetail="toDetail" />
+      </div>
+    </template>
+  </el-skeleton>
 </template>
 
 <script setup>
-import videoCover from '@/views/Aside/video/children/videoCover.vue'
-import {computed,ref} from "vue";
-import {useRouter} from "vue-router";
-import {useStore} from "vuex";
-import {getSearchResult} from "@/network/search.js";
-import {formatVideoData} from "@/utlis/formatData.js";
+import horizontalCover from '@/views/Aside/video/components/horizontalCover.vue'
+import { computed, ref, reactive, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { getSearchResult } from '@/network/search.js'
+import { formatVideoData } from '@/utlis/formatData.js'
 
-
-let video = ref([])
+const videoArray = ref([])
 
 const store = useStore()
 const keywords = computed(() => store.state.songDetail.keywords)
 
-
-let params = {
-  keywords:keywords.value,
-  type:1014,
-  limit:16
-}
-getSearchResult(params).then(res => {
-  video.value = formatVideoData(res.data.result.videos)
+const params = reactive({
+  keywords: keywords.value,
+  type: 1014,
+  limit: 16
 })
+
+onMounted(() => {
+  getSearchResult(params).then(res => {
+    videoArray.value = formatVideoData(res.data.result.videos)
+  })
+})
+
 const router = useRouter()
-const goDetail = id => {
+const toDetail = id => {
   router.push(`/videoDetail?${id.length < 20 ? 'id' : 'vid'}=${id}`)
 }
 
 </script>
 
 <style scoped lang="less">
+  .video-box {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
 
+    .skeleton-item-image {
+      width: 285px;
+      height: 150px;
+      margin-top: 20px;
+    }
+    .skeleton-item-p {
+      height: 20px;
+      width: 285px;
+    }
+    .skeleton-item-p:nth-child(1) {
+      margin-top: 5px;
+    }
+  }
 </style>
