89983a29b3a5b43b1796d89e9075eb6cbe77d5f4
靳少辉 <1051131737@qq.com>
2022-05-05 17:27:12 +0800

    路由 - route.meta.name代替route.name防止name重复

--- src/router/index.js
+++ src/router/index.js
@@ -6,28 +6,15 @@ const routes = [
   },
   {
     path: '/search',
-    component: () => import('@/views/Search/Search.vue'),
+    meta: { name: '搜索' },
+    redirect: '/search/song',
+    component: () => import('@/views/Search/index.vue'),
     children: [
-      {
-        path: '',
-        component: () => import('@/views/Search/pages/song.vue')
-      },
-      {
-        path: 'singer',
-        component: () => import('@/views/Search/pages/singer.vue')
-      },
-      {
-        path: 'album',
-        component: () => import('@/views/Search/pages/album.vue')
-      },
-      {
-        path: 'video',
-        component: () => import('@/views/Search/pages/video.vue')
-      },
-      {
-        path: 'songMenu',
-        component: () => import('@/views/Search/pages/songMenu.vue')
-      }
+      { path: 'song', meta: { name: '单曲' }, component: () => import('@/views/Search/pages/song.vue') },
+      { path: 'singer', meta: { name: '歌曲' }, component: () => import('@/views/Search/pages/singer.vue') },
+      { path: 'album', meta: { name: '专辑' }, component: () => import('@/views/Search/pages/album.vue') },
+      { path: 'video', meta: { name: '视频' }, component: () => import('@/views/Search/pages/video.vue') },
+      { path: 'songMenu', meta: { name: '歌单' }, component: () => import('@/views/Search/pages/songMenu.vue') }
     ]
   },
   {
@@ -36,90 +23,90 @@ const routes = [
   },
   {
     path: '/findMusic',
-    name: '发现音乐',
+    meta: { name: '发现音乐' },
     component: () => import('@/views/Aside/findMusic/index.vue'),
     children: [
-      { path: 'recommend', name: '个性推荐', component: () => import('@/views/Aside/findMusic/pages/recommend/index.vue') },
-      { path: 'exclusive', name: '专属定制', component: () => import('@/views/Aside/findMusic/pages/exclusive/index.vue') },
-      { path: 'songMenu', name: '歌单', component: () => import('@/views/Aside/findMusic/pages/songMenu/index.vue') },
-      { path: 'leaderboard', name: '排行榜', component: () => import('@/views/Aside/findMusic/pages/leaderboard/index.vue') },
-      { path: 'singer', name: '歌手', component: () => import('@/views/Aside/findMusic/pages/singer/index.vue') },
+      { path: 'recommend', meta: { name: '个性推荐' }, component: () => import('@/views/Aside/findMusic/pages/recommend/index.vue') },
+      { path: 'exclusive', meta: { name: '专属定制' }, component: () => import('@/views/Aside/findMusic/pages/exclusive/index.vue') },
+      { path: 'songMenu', meta: { name: '歌单' }, component: () => import('@/views/Aside/findMusic/pages/songMenu/index.vue') },
+      { path: 'leaderboard', meta: { name: '排行榜' }, component: () => import('@/views/Aside/findMusic/pages/leaderboard/index.vue') },
+      { path: 'singer', meta: { name: '歌手' }, component: () => import('@/views/Aside/findMusic/pages/singer/index.vue') },
       {
         path: 'newMusic',
         name: '最新音乐',
         component: () => import('@/views/Aside/findMusic/pages/newMusic/index.vue'),
         redirect: '/findMusic/newMusic/song',
         children: [
-          { path: 'song', name: '新歌速递', component: () => import('@/views/Aside/findMusic/pages/newMusic/pages/song/index.vue') },
-          { path: 'disc', name: '新碟上架', component: () => import('@/views/Aside/findMusic/pages/newMusic/pages/disc/index.vue') }
+          { path: 'song', meta: { name: '新歌速递' }, component: () => import('@/views/Aside/findMusic/pages/newMusic/pages/song/index.vue') },
+          { path: 'disc', meta: { name: '新碟上架' }, component: () => import('@/views/Aside/findMusic/pages/newMusic/pages/disc/index.vue') }
         ]
       }
     ]
   },
   {
     path: '/video',
-    name: '视频MV',
+    meta: { name: '视频MV' },
     redirect: '/video/video',
     component: () => import('@/views/Aside/video/index.vue'),
     children: [
-      { path: 'video', component: () => import('@/views/Aside/video/pages/video/index.vue') },
-      { path: 'mv', component: () => import('@/views/Aside/video/pages/mv/index.vue') },
-      { path: 'allMV', component: () => import('@/views/Aside/video/pages/mv/all.vue') },
-      { path: 'rank', component: () => import('@/views/Aside/video/pages/rank/index.vue') }
+      { path: 'video', meta: { name: '视频' }, component: () => import('@/views/Aside/video/pages/video/index.vue') },
+      { path: 'mv', meta: { name: 'MV' }, component: () => import('@/views/Aside/video/pages/mv/index.vue') },
+      { path: 'allMV', meta: { name: '全部MV' }, component: () => import('@/views/Aside/video/pages/mv/all.vue') },
+      { path: 'rank', meta: { name: 'MV排行榜' }, component: () => import('@/views/Aside/video/pages/rank/index.vue') }
     ]
   },
   {
     path: '/podcast',
-    name: '播客',
+    meta: { name: '播客' },
     redirect: '/podcast/radioCenter',
     component: () => import('@/views/Aside/podcast/index.vue'),
     children: [
-      { path: 'radioCenter', name: '电台中心', component: () => import('@/views/Aside/podcast/pages/radioCenter/index.vue') },
-      { path: 'voiceRank', name: '声音榜', component: () => import('@/views/Aside/podcast/pages/voiceRank/index.vue') },
-      { path: 'freshRank', name: '新晋播客榜', component: () => import('@/views/Aside/podcast/pages/freshRank/index.vue') },
-      { path: 'hotRank', name: '热门播客榜', component: () => import('@/views/Aside/podcast/pages/hotRank/index.vue') },
-      { path: 'hourRank', name: '24小时节目榜', component: () => import('@/views/Aside/podcast/pages/hourRank/index.vue') }
+      { path: 'radioCenter', meta: { name: '电台中心' }, component: () => import('@/views/Aside/podcast/pages/radioCenter/index.vue') },
+      { path: 'voiceRank', meta: { name: '声音榜' }, component: () => import('@/views/Aside/podcast/pages/voiceRank/index.vue') },
+      { path: 'freshRank', meta: { name: '新晋播客榜' }, component: () => import('@/views/Aside/podcast/pages/freshRank/index.vue') },
+      { path: 'hotRank', meta: { name: '热门播客榜' }, component: () => import('@/views/Aside/podcast/pages/hotRank/index.vue') },
+      { path: 'hourRank', meta: { name: '24小时节目榜' }, component: () => import('@/views/Aside/podcast/pages/hourRank/index.vue') }
     ]
   },
   {
     path: '/friend',
-    name: '动态',
+    meta: { name: '动态' },
     component: () => import('@/views/Aside/friend/index.vue')
   },
   {
     path: '/fm',
-    name: '私人FM',
+    meta: { name: '私人FM' },
     component: () => import('@/views/Aside/fm/index.vue')
   },
   {
     path: '/recentPlay',
-    name: '最近播放',
+    meta: { name: '最近播放' },
     component: () => import('@/views/Aside/recentPlay/index.vue')
   },
   {
     path: '/myPodcast',
-    name: '我的播客',
+    meta: { name: '我的播客' },
     component: () => import('@/views/Aside/myPodcast/index.vue')
   },
   {
     path: '/myCollect',
-    name: '我的收藏',
+    meta: { name: '我的收藏' },
     redirect: '/myCollect/album',
     component: () => import('@/views/Aside/myCollect/index.vue'),
     children: [
-      { path: 'album', name: '专辑', component: () => import('@/views/Aside/myCollect/pages/album/index.vue') },
-      { path: 'singer', name: '歌手', component: () => import('@/views/Aside/myCollect/pages/singer/index.vue') },
-      { path: 'video', name: '视频', component: () => import('@/views/Aside/myCollect/pages/video/index.vue') }
+      { path: 'album', meta: { name: '专辑' }, component: () => import('@/views/Aside/myCollect/pages/album/index.vue') },
+      { path: 'singer', meta: { name: '歌手' }, component: () => import('@/views/Aside/myCollect/pages/singer/index.vue') },
+      { path: 'video', meta: { name: '视频' }, component: () => import('@/views/Aside/myCollect/pages/video/index.vue') }
     ]
   },
   {
     path: '/myLike',
-    name: '我喜欢的音乐',
+    meta: { name: '我喜欢的音乐' },
     component: () => import('@/views/Aside/myLike/index.vue')
   },
   {
     path: '/mySongList',
-    name: '我的歌单',
+    meta: { name: '我的歌单' },
     component: () => import('@/views/Aside/mySongList/index.vue')
   },
   {
