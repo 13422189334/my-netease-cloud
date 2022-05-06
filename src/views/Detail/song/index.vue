54133826e341a740a2c1458d8a3c091eb4f10cd2
靳少辉 <1051131737@qq.com>
2022-05-06 18:06:50 +0800

    详情 - 歌单详情 - 中间页重构

--- src/views/Detail/song/index.vue
+++ src/views/Detail/song/index.vue
@@ -1,33 +1,30 @@
 <template>
   <dailyTop v-if="$store.state.songDetail.isDaily" />
-  <!--    歌单顶部-->
   <songTop v-else />
-  <!--    分隔线-->
   <el-divider content-position="right">网易云音乐</el-divider>
-  <!--  tabs-->
   <el-menu
     router
     :default-active="$route.path"
     mode="horizontal"
   >
-    <el-menu-item index="/songDetail">歌单列表</el-menu-item>
-    <el-menu-item index="/songDetail/comment">评论 ({{ count }})</el-menu-item>
-    <el-menu-item index="/songDetail/collect">收藏者</el-menu-item>
+    <el-menu-item index="/detail/song">歌单列表</el-menu-item>
+    <el-menu-item index="/detail/song/comment">评论 ({{ count }})</el-menu-item>
+    <el-menu-item index="/detail/song/collect">收藏者</el-menu-item>
   </el-menu>
 
   <router-view />
 </template>
 
 <script setup>
+import dailyTop from './component/dailyTop.vue'
+import songTop from './component/songTop.vue'
 import { computed, ref, watch } from 'vue'
 import { useStore } from 'vuex'
 import { getComment } from '@/network/comment.js'
 
-import dailyTop from './component/dailyTop.vue'
-import songTop from './component/songTop.vue'
-
 const store = useStore()
 const id = computed(() => store.state.songDetail.commentID)
+
 const count = ref(0)
 
 watch(id, newID => {
@@ -41,7 +38,3 @@ watch(id, newID => {
   })
 }, { immediate: true })
 </script>
-
-<style scoped lang="less">
-
-</style>
