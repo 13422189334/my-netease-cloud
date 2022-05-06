b7875662900e43b4975f693d39be7599ce99368a
靳少辉 <1051131737@qq.com>
2022-05-06 18:05:15 +0800

    详情 - 音乐table组件 - 重构

--- src/views/Detail/song/pages/SongTable.vue
+++ src/views/Detail/song/pages/SongTable.vue
@@ -1,29 +1,31 @@
 <template>
-  <!--歌曲列表-->
   <el-table
-    :data="tableData"
+    :data="songArray"
     stripe
     size="small"
     highlight-current-row
-    style="width: 100%;"
     @row-dblclick="handleCurrentChange"
   >
     <el-table-column width="45">
       <template #default="scope">
-        <svg v-if="scope.row.id === playName" style="width: 15px;height: 15px;" class="svg" aria-hidden="true"><use xlink:href="#icon-yangshengqi" /></svg>
+        <svg v-if="scope.row.id === currentId" class="id" aria-hidden="true">
+          <use xlink:href="#icon-yangshengqi" />
+        </svg>
         <span v-else>{{ scope.row.index }}</span>
       </template>
     </el-table-column>
-    <el-table-column ref="dom" width="45">
+    <el-table-column width="45">
       <template #default="scope">
-        <i v-if="isLike(scope.row.id)" style="color: red; font-size: 14px;" class="iconfont icon-xihuan" />
-        <i v-else title="喜欢" class="iconfont icon-aixin" style="font-size: 15px;" />
+        <i v-if="isLike(scope.row.id)" class="iconfont icon-xihuan icon red-icon" />
+        <i v-else title="喜欢" class="iconfont icon-aixin icon" />
       </template>
     </el-table-column>
-    <el-table-column width="40"><i title="下载" class="iconfont icon-xiazai" style="font-size: 15px;" /></el-table-column>
+    <el-table-column width="40">
+      <i title="下载" class="iconfont icon-xiazai icon"/>
+    </el-table-column>
     <el-table-column prop="name" label="音乐标题" width="400">
       <template #default="scope">
-        <span class="music-title" :class="{active:scope.row.id === playName}">{{ scope.row.name }}</span>
+        <span class="mr-5" :class="{ active: scope.row.id === currentId }">{{ scope.row.name }}</span>
         <el-tag v-if="scope.row.mv" type="danger" size="mini" @click="toDetail(scope.row.mv)">MV</el-tag>
       </template>
     </el-table-column>
@@ -46,47 +48,67 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
-// import { getLikeMusic } from '@/network/song.js'
 import eventBus from '@/utlis/eventbus.js'
 
 const store = useStore()
 const router = useRouter()
 
-const tableData = computed(() => store.state.songDetail.songArray)
-const playDetail = computed(() => store.state.songDetail.songDetail.id)
-const current = ref(-1)
-const handleCurrentChange = row => {
-  if (row.id === playDetail.value) return
-  const index = tableData.value.findIndex(item => item.id === row.id)
-  current.value = index
+const songArray = computed(() => store.state.songDetail.songArray)
+const currentId = computed(() => store.state.songDetail.songDetail.id)
+
+/**
+ * 双击事件
+ * */
+const handleCurrentChange = (row) => {
+  if (row.id === currentId.value) return
+  const index = songArray.value.findIndex(item => item.id === row.id)
   store.commit('setSongDetail', row)
   store.commit('play', index)
   eventBus.emit('playMusic')
 }
 
+/**
+ * 跳转详情
+ */
 const toDetail = id => {
   router.push(`/videoDetail?id=${id}`)
 }
 
-const playName = computed(() => store.state.songDetail.songDetail.id)
-
+/**
+ * 是否是收藏歌曲
+ * @param id
+ * @returns {boolean}
+ */
 const isLike = id => store.state.singer.likeMusic.includes(id)
 
 eventBus.on('playAll', () => {
-  handleCurrentChange(tableData.value[0])
+  handleCurrentChange(songArray.value[0])
 })
 </script>
 
 <style scoped lang="less">
-.active{
-  color: red;
-  font-weight: 900;
-}
-.music-title{
-  margin-right: 5px;
-}
+  .active {
+    color: red;
+    font-weight: 900;
+  }
 
+  .mr-5 {
+    margin-right: 5px;
+  }
+
+  .id {
+    width: 15px;
+    height: 15px;
+  }
+
+  .icon {
+    font-size: 15px;
+  }
+
+  .red-icon {
+    color: red;
+  }
 </style>
