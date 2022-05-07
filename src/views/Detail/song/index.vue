577236fdf07e3740469dc28f1a72770112e71171
靳少辉 <1051131737@qq.com>
2022-05-07 11:27:01 +0800

    详情 - 歌单中间页 - 逻辑调整

--- src/views/Detail/song/index.vue
+++ src/views/Detail/song/index.vue
@@ -8,8 +8,8 @@
     mode="horizontal"
   >
     <el-menu-item index="/detail/song">歌单列表</el-menu-item>
-    <el-menu-item index="/detail/song/comment">评论 ({{ count }})</el-menu-item>
-    <el-menu-item index="/detail/song/collect">收藏者</el-menu-item>
+    <el-menu-item index="/detail/song/comment">评论 ({{ comment }})</el-menu-item>
+    <el-menu-item index="/detail/song/collect">收藏者 ({{ collect }})</el-menu-item>
   </el-menu>
 
   <router-view />
@@ -18,23 +18,39 @@
 <script setup>
 import dailyTop from './component/dailyTop.vue'
 import songTop from './component/songTop.vue'
-import { computed, ref, watch } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import { useStore } from 'vuex'
 import { getComment } from '@/network/comment.js'
+import { getSongListCollect } from '@/network/songList.js'
+import eventBus from '@/utlis/eventbus.js'
 
 const store = useStore()
 const id = computed(() => store.state.songDetail.commentID)
 
-const count = ref(0)
+const comment = ref(0)
+const collect = ref(0)
 
-watch(id, newID => {
+const getTotal = () => {
   const params = {
-    id: newID,
+    id: id.value,
     type: 2,
-    limit: 0
+    limit: 15,
+    offset: 0
   }
   getComment(params).then(res => {
-    count.value = res.data.total
+    comment.value = res.data.total
   })
-}, { immediate: true })
+  getSongListCollect(params).then(res => {
+    console.log(res)
+    collect.value = res.data.total
+  })
+}
+
+onMounted(() => {
+  getTotal()
+})
+
+eventBus.on('get-song-detail', () => {
+  getTotal()
+})
 </script>
