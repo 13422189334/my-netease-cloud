41c1ddb176c2eb1ecbcfe8891b6f9acc0bd2b9c7
靳少辉 <1051131737@qq.com>
2022-05-07 11:28:41 +0800

    详情 - 歌单收藏详情 - 代码重构

--- src/views/Detail/song/pages/Collect.vue
+++ src/views/Detail/song/pages/Collect.vue
@@ -1,9 +1,7 @@
 <template>
   <section class="collect">
     <main v-for="item in collect" :key="item.userId">
-      <div>
-        <el-avatar :size="100" :src="item.avatarUrl" />
-      </div>
+      <el-avatar :size="100" :src="item.avatarUrl" />
       <div class="user">
         <p>{{ item.nickname }}</p>
         <p class="label">{{ item.signature }}</p>
@@ -23,7 +21,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, reactive } from 'vue'
 import { useStore } from 'vuex'
 import { getSongListCollect } from '@/network/songList.js'
 
@@ -31,50 +29,61 @@ const store = useStore()
 const id = computed(() => store.state.songDetail.commentID)
 
 const collect = ref([])
-const params = {
+const params = reactive({
   id: id.value,
   limit: 15,
   offset: 0
-}
-const total = ref()
-onMounted(async() => {
-  const res = await getSongListCollect(params)
-  collect.value = res.data.subscribers
-  total.value = res.data.total
 })
-const change = value => {
-  params.offset = value
+const total = ref()
+
+/**
+   * 获取收藏信息
+   */
+const getData = () => {
   getSongListCollect(params).then(res => {
     collect.value = res.data.subscribers
+    total.value = res.data.total
   })
+}
+
+onMounted(async() => {
+  getData()
+})
+
+const change = value => {
+  params.offset = value
+  getData()
 }
 </script>
 
 <style scoped lang="less">
-.collect{
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  main{
+  .collect {
     display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: 33%;
-    height: 100%;
-    margin: 10px 0;
-    .user{
-      margin-left: 15px;
-      .label{
-        color: silver;
-        font-size: 13px;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    main {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      width: 33%;
+      height: 100%;
+      margin: 10px 0;
+
+      .user {
+        margin-left: 15px;
+
+        .label {
+          color: silver;
+          font-size: 13px;
+        }
       }
     }
   }
-}
 
-.pages{
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
+  .pages {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 </style>
