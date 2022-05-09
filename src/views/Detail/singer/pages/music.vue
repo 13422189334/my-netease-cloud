e3c4d583e30b4a8f39431bb9ebc07b592aa6c485
靳少辉 <1051131737@qq.com>
2022-05-09 17:19:42 +0800

    详情 - 歌手详情 - 歌手单曲页面重构

--- src/views/Detail/singer/pages/music.vue
+++ src/views/Detail/singer/pages/music.vue
@@ -4,18 +4,14 @@
 
 <script setup>
 import SongTable from '@/views/Detail/song/pages/SongTable.vue'
-import { onMounted, computed } from 'vue'
+import { computed, watch } from 'vue'
 import { useStore } from 'vuex'
 import { getSingerSong } from '@/network/singer.js'
 
 const store = useStore()
 const id = computed(() => store.state.singer.singerId)
-onMounted(async() => {
-  const res = await getSingerSong(id.value)
+watch(id, async(val) => {
+  const res = await getSingerSong(val)
   store.commit('setSongMusic', res.data.songs)
-})
+}, { immediate: true, deep: true })
 </script>
-
-<style scoped lang="less">
-
-</style>
